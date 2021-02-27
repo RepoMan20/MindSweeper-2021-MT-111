80427bc760dc0c936a3b90c5db8d41fe8950d225
theguywithcode <mehulshah0530@gmail.com>
2021-02-27 21:31:56 +0530

    DataBase_Thik_ho_gaya_finally

--- advance_appointment/home/admin.py
+++ advance_appointment/home/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from home.models import *
-admin.site.register(Patients_login)
+admin.site.register(Patients_appointment_Details)
+admin.site.register(Doctor_Details)
--- advance_appointment/home/models.py
+++ advance_appointment/home/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User , auth
 # Create your models here.
-
-class Patients_login(models.Model):
-    Email = models.EmailField(max_length=254)
-    password = models.CharField(max_length=50)
+class Patients_appointment_Details(models.Model):
+    Patient_id = models.IntegerField()
+    Patient_name = models.CharField(max_length=255)
+    Serious_illness = models.TextField()
+    Allergies = models.TextField()
+    Medical_history = models.TextField()
+    Appointment_start = models.TimeField()
+    Appointment_End = models.TextField()
+    Priority = models.IntegerField()
     class Meta:
-        db_table="Patient_details"
+        db_table="Patient_Appointment_details"
+    def str(self):
+        return self.Patient_name
+
+
+class Doctor_Details(models.Model):
+    Patient_id = models.IntegerField()
+    Appointment_id = models.IntegerField()
+    patient_name = models.CharField(max_length=255)
+    Appointment_start = models.TimeField()
+    Appointment_End = models.TextField()
+    Priority = models.IntegerField()
+    class Meta:
+        db_table = "Doctor_details"
